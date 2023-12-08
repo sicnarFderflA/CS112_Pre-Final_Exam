12137c08b825d994987560b09216be4285636680
sicnarFderflA <144906631+sicnarFderflA@users.noreply.github.com>
2023-12-08 08:59:50 +0800

    Rabanes_Pre-Final_Exam.py
    Additional Conditions.
    added more checkers to display the list of prime if start and end is inputted correctly

--- Rabanes_Pre-Final_Exam.py
+++ Rabanes_Pre-Final_Exam.py
@@ -13,46 +13,58 @@ print("CS112: COMPUTER PROGRAMMING 1 - PRE-FINAL EXAM"
  
 while True:
     start = float(input("Enter a number [start]: "))
+    check1 = True
     if start < 0:
         print("Enter a non-negative number.")
+        check1 = False
         continue
     elif start == 0:
         print("Program Terminated.")
+        check1 = False
         break
     elif not start.is_integer():
         print("Enter an integer number only.")
+        check1 = False
         continue
-    
+
     while True:
         end = float(input("Enter a number [end]: "))
+        check2 = True
         if end < 0:
             print("Enter a non-negative number.")
+            check2 = False
+            continue
         elif end == 0:
             print("Program Terminated.")
+            check2 = False
             break
         elif end <= start:
             print(f"Enter a number greater than {start}."
                   "\n")
+            check2 = False
             continue
         elif not end.is_integer():
             print("Enter an integer number only.")
+            check2 = False
             continue
-
+        
         start = int(start)
         end = int(end)
         result = ""
-        if start == 1:
-            for i in range(2, end+1):
-                if i % 2 != 0:
+        if start <= 2 <= end: #include 2 if its in the range
+            result += "2 " # since 2 % 2 = 0 therefore the code will make 2 as not a prime number.
+        if start == 1: 
+            for i in range(2, end+1): # 1 is a special number so we skip it
+                if i % 2 != 0: # checks if the number is uneven. p.s not all uneven numbers are prime but prime numbers except for 2 are uneven.
                     result += str(i) + " "
         else:
-            for i in range(start, end+1):
+            for i in range(start, end+1): 
                 if i % 2 != 0:
                     result += str(i) + " "
 
-
-        print(f"The Prime numbers between {start} and {end} is {result}"
-              '\n')
-        break
+        if check1 and check2 == True:
+            print(f"The Prime numbers between {start} and {end} is {result}"
+                '\n')
+            break
     if end == 0:
         break
